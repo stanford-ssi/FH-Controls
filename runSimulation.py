e240331ace1300f53dd12706bb3403242332eeb8
Jacob Rintamaki <jrin@stanford.edu>
2023-11-07 08:59:35 -0800

    Update runSimulation.py

--- runSimulation.py
+++ runSimulation.py
@@ -9,6 +9,34 @@ state_0 = np.array([0, 0, 0, 0, 0, 0]) # Start State
 tf = 10
 ts = 0.1
 
+#forgive me for I have sinned 
+g = 9.81  # m/s^2, acceleration due to gravity
+dt = 0.2  # seconds, time step
+N = 100  # number of time steps
+
+# Derived constants
+max_a_rate_up = 0.1 * g * dt # m/s^2, maximum upward acceleration
+max_a_rate_down = -0.1 * g * dt # m/s^2, maximum downward acceleration 
+h_max = 55  # m, maximum height
+h_min = 0  # m, minimum height
+v_max = 10  # m/s, maximum velocity
+v_min = -10  # m/s, minimum velocity
+a_max = g  # m/s^2, maximum acceleration
+a_min = -g  # m/s^2 minimum acceleration
+h_target = 50  # m, target height
+v_target = 0  # m/s, target velocity
+x0 = np.array([0, 0, 0])  # [m, m, m], initial state vector
+
+# Model matrices
+Phi = np.array([[1, dt], [0, 1]])
+Gamma = np.array([0, dt])
+
+
+
+
+
+
+
 # Setup Simulation
 
 #variables used for planned trajectory come from pathPlannerConstants (this could cause issues, and is something I want to rework)

376e5cceb42b6b58cfa3fa987197ebf9e023ce76
Jacob Rintamaki <jrin@stanford.edu>
2023-11-07 09:31:28 -0800

    trying some things

--- PathPlanner/plan.py
+++ PathPlanner/plan.py
@@ -19,12 +19,19 @@ class PlannedTrajectory:
         self.a_min = a_min
         self.max_a_rate_up = max_a_rate_up
         self.max_a_rate_down = max_a_rate_down
+        
+        # Pre-compute Phi and Gamma matrices
+        self.Phi = np.array([[1, dt, 0.5 * dt**2], [0, 1, dt], [0, 0, 1]])
+        self.Gamma = np.array([dt**2 / 2, dt, 1]).reshape((3,1))
+
+        # Initialize linear programming vectors and matrices
         self.f = np.zeros(3 * N)
         self.A = np.zeros((6 * N, 3 * N))
         self.B = np.zeros(6 * N)
         self.A_eq = np.zeros((2 * N + 4, 3 * N))
         self.B_eq = np.zeros(2 * N + 4)
         self.result = None
+        
         self.setup_problem()
 
     def setup_problem(self):
@@ -50,25 +57,21 @@ class PlannedTrajectory:
         for i in range(1, self.N):
             idx = 2 * (i - 1) + 6 * self.N
             self.A[idx, 3 * i] = 1
-            self.A[idx, 3 * (i + 1)] = -1
-            self.B[idx] = -self.max_a_rate_down
+            self.A[idx, 3 * (i - 1)] = -1
+            self.B[idx] = self.dt * self.max_a_rate_down
             self.A[idx + 1, 3 * i] = -1
-            self.A[idx + 1, 3 * (i + 1)] = 1
-            self.B[idx + 1] = self.max_a_rate_up
+            self.A[idx + 1, 3 * (i - 1)] = 1
+            self.B[idx + 1] = self.dt * self.max_a_rate_up
 
     def setup_equality_constraints(self):
         # Initialize and set up equality constraints A_eq and B_eq
-        physics_mat = np.zeros((3, 3))
-        physics_mat[:2, :2] = Phi
-        physics_mat[:2, 2] = Gamma
-
         for i in range(self.N - 1):
             idx = 2 * i
-            self.A_eq[idx:idx+2, 3*i:3*i+3] = -physics_mat[:2, :3]
+            self.A_eq[idx:idx+2, 3*i:3*i+3] = -self.Phi[:2, :3]
             self.A_eq[idx:idx+2, 3*(i+1):3*(i+1)+2] = np.eye(2)
 
-        self.A_eq[2 * self.N - 1:2 * self.N + 1, :3] = np.eye(3)
-        self.B_eq[2 * self.N - 1:2 * self.N + 1] = self.x0
+        self.A_eq[2 * self.N - 2:2 * self.N, :3] = np.eye(3)
+        self.B_eq[2 * self.N - 2:2 * self.N] = self.x0
 
         self.A_eq[2 * self.N + 2, 3*self.N-3] = 1
         self.A_eq[2 * self.N + 3, 3*self.N-2] = 1
@@ -80,8 +83,7 @@ class PlannedTrajectory:
         # Since scipy.optimize.linprog minimizes, we negate f to maximize
         self.result = linprog(-self.f, A_ub=self.A, b_ub=self.B, A_eq=self.A_eq, b_eq=self.B_eq, method='highs')
 
-    #This was just from vignesh's original matlab code but this doesn't get used at the moment. 
-    #TO-DO: Talk with Jerry AND Vignesh about how to integrate this into our existing plots and what is relevant vs not-relevant.
+    #not used at the moment 
     def plot_results(self):
         # Extract the solution and plot the results
         x = self.result.x

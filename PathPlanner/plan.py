366ade49b6e9efab256bb638d342401b4cea2aed
Jacob Rintamaki <jrin@stanford.edu>
2023-11-28 13:43:46 -0800

    Update plan.py

--- PathPlanner/plan.py
+++ PathPlanner/plan.py
@@ -3,13 +3,22 @@ from scipy.optimize import linprog
 import matplotlib.pyplot as plt
 import Graphing.plotter
 
-class PlannedTrajectory:
-    def __init__(self, max_altitude, time, target, dt):
-        self.max_altitude = max_altitude
-        self.target = target
-        self.dt = dt
-        self.t_total = time
-        self.trajectory = self.cubic_trajectory()
-    def plan_path(self):  
-        
+H_value_updated = 50  # New maximum height
+
+# Recalculate the height values with the updated maximum height
+h1_values_updated = np.array([-16*H_value_updated/T_value**3 * t**3 + 12*H_value_updated/T_value**2 * t**2 for t in t_ascent])
+h2_values_updated = np.array([16*H_value_updated/T_value**3 * t**3 - 36*H_value_updated/T_value**2 * t**2 + 24*H_value_updated/T_value * t - 4*H_value_updated for t in t_descent])
+
+# Combine the ascent and descent for plotting
+h_combined_updated = np.concatenate([h1_values_updated, h2_values_updated])
+
+# Plotting the updated trajectory
+plt.figure(figsize=(10, 6))
+plt.plot(t_combined, h_combined_updated, label='Height vs Time')
+plt.title('Cubic Ascent and Descent Trajectory (50m Height)')
+plt.xlabel('Time (s)')
+plt.ylabel('Height (m)')
+plt.grid(True)
+plt.legend()
+plt.show()
     

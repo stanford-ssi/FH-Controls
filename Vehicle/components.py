de29686173b1eef98758d76279bad4b3126b3639
gia ancone <giaancone@DN0a24a138.SUNet>
2023-12-05 20:25:31 -0800

    Moment of Inertia

---
+++ Vehicle/components.py
@@ -0,0 +1,201 @@
+from abc import abstractmethod, ABC
+from enum import Enum, auto
+from typing import Dict
+
+
+
+class ComponentType(Enum, ABC):
+
+    components = {}
+
+    @abstractmethod
+    def moment_of_inertia_xy(self):
+        pass
+
+    @abstractmethod
+    def moment_of_inertia_z(self):
+        pass
+
+    @abstractmethod    
+    def center_of_mass(self):
+        pass
+
+    class HollowCylinder(Enum):
+        #Component List Structure: [mass, inner radius, outer_radius, length, bottom_z]
+        def moment_of_inertia_xy(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about an axis in the X-Y plane going through its center of mass.
+            Format: [mass, inner radius, outer_radius, length, bottom_z]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            inner_radius = hollow_vars[1]
+            outer_radius = hollow_vars[2]
+            length = hollow_vars[3]
+            moi_xy = (mass / 12) * ((3 * ((outer_radius ** 2) + (inner_radius ** 2))) + length ** 2)
+
+        def moment_of_inertia_z(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about Z Axis.
+            Format: [mass, inner radius, outer_radius, length, bottom_z]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            inner_radius = hollow_vars[1]
+            outer_radius = hollow_vars[2]
+            return 0.5 * mass * (inner_radius ** 2 + outer_radius ** 2)
+        
+        def center_of_mass(self):
+            '''
+            Determines the center of mass of a hollow cylinder.
+            Return value format: [z_coord, mass]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            bottom_z = hollow_vars[4]
+            length = hollow_vars[3]
+            z_coord = bottom_z + length / 2
+            return [z_coord, mass]
+
+
+
+    class SolidCylinder(Enum):
+        #Component List Structure: [mass, radius, length, bottom_z]
+        def moment_of_inertia_xy(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about an axis in the X-Y plane going through its center of mass.
+            Format: [mass, radius, length, bottom_z]
+            '''
+            solid_vars = ComponentType.components[ComponentType.SolidCylinder]
+            mass = solid_vars[0]
+            radius = solid_vars[1]
+            length = solid_vars[2]
+            moi_xy = (0.25 * mass * (radius ** 2)) + (mass * (length ** 2) / 12)
+            return moi_xy
+
+        def moment_of_inertia_z(self):
+            '''
+            Determines moment of Inertia of a Solid Cylinder/Disk about Z Axis.-
+            Format solid_vars: [mass, radius, length, bottom_z]
+            '''
+            solid_vars = ComponentType.components[ComponentType.SolidCylinder]
+            mass = solid_vars[0]
+            radius = solid_vars[1]
+            return 0.5 * mass * (radius ** 2)
+        
+        def center_of_mass(self):
+            '''
+            Determines the center of mass of a hollow cylinder.
+            Return value format: [z_coord, mass]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            bottom_z = hollow_vars[3]
+            length = hollow_vars[2]
+            z_coord = bottom_z + length / 2
+            return [z_coord, mass]
+        
+            
+
+    class ChangingHollowCylinder(Enum):
+        # Component List Structure: [mass, current_inner_radius, outer_radius, length, bottom_z, original_inner_radius]
+        def moment_of_inertia_xy(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about an axis in the X-Y plane going through its center of mass.
+            Format: [mass, inner radius, outer_radius, length, bottom_z]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            inner_radius = hollow_vars[1]
+            outer_radius = hollow_vars[2]
+            length = hollow_vars[3]
+            moi_xy = (mass / 12) * ((3 * ((outer_radius ** 2) + (inner_radius ** 2))) + length ** 2)
+
+        def moment_of_inertia_z(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about Z Axis.
+            Format: [mass, inner radius, outer_radius, length, bottom_z]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.ChangingHollowCylinder]
+            mass = hollow_vars[0]
+            inner_radius = hollow_vars[1]
+            outer_radius = hollow_vars[2]
+            return 0.5 * mass * (inner_radius ** 2 + outer_radius ** 2)
+        
+        def center_of_mass(self):
+            '''
+            Determines the center of mass of a hollow cylinder.
+            Return value format: [z_coord, mass]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            bottom_z = hollow_vars[4]
+            length = hollow_vars[3]
+            z_coord = bottom_z + length / 2
+            return [z_coord, mass]
+
+    class ChangingSolidCylinder(Enum):
+        # Component List Structure: [mass, radius, current length, bottom_z, original_length]
+        def moment_of_inertia_xy(self):
+            '''
+            Determines moment of Inertia of a Hollow Cylinder about an axis in the X-Y plane going through its center of mass.
+            Format: [mass, radius, length, bottom_z]
+            '''
+            solid_vars = ComponentType.components[ComponentType.ChangingSolidCylinder]
+            mass = solid_vars[0]
+            radius = solid_vars[1]
+            length = solid_vars[2]
+            moi_xy = (0.25 * mass * (radius ** 2)) + (mass * (length ** 2) / 12)
+            return moi_xy
+
+        def moment_of_inertia_z(self):
+            '''
+            Determines moment of Inertia of a Solid Cylinder/Disk about Z Axis.-
+            Format solid_vars: [mass, radius, length, bottom_z]
+            '''
+            solid_vars = ComponentType.components[ComponentType.ChangingSolidCylinder]
+            mass = solid_vars[0]
+            radius = solid_vars[1]
+            return 0.5 * mass * (radius ** 2)
+        
+        def center_of_mass(self):
+            '''
+            Determines the center of mass of a hollow cylinder.
+            Return value format: [z_coord, mass]
+            '''
+            hollow_vars = ComponentType.components[ComponentType.HollowCylinder]
+            mass = hollow_vars[0]
+            bottom_z = hollow_vars[3]
+            length = hollow_vars[2]
+            z_coord = bottom_z + length / 2
+            return [z_coord, mass]
+
+    class PointMass(Enum):
+        # Component List Structure: [mass, bottom_z]
+        def moment_of_inertia_xy(self):
+            '''
+            Returns component list for point mass.  Returns 0 because the Parallel Axis Theorem will compute the XY MOI in the total_moi_xy() function.
+            '''
+            return 0
+
+        def moment_of_inertia_z(self):
+            '''
+            Assumes point mass is along z-axis.
+            Returns Moment of Inertia of 0
+            '''
+            return 0
+        
+        def center_of_mass(self):
+            '''
+            Return Center of Mass of Point Mass
+            Return value format: [z_coord, mass]
+            '''
+            lst = ComponentType.components[ComponentType.PointMass]
+            return [lst[1], lst[0]]
+
+
+
+
+
+
+
---
+++ Vehicle/momentOfInertia.py
@@ -0,0 +1,117 @@
+import math
+import numpy as np
+from Vehicle.engine import Engine
+from components import ComponentType
+
+
+def total_moi_xy():
+    """
+    Calculates the total moment of inertia about the center of mass in the XY Plane, using the parallel axis theorem.  
+    center_of_mass() outputs an individual component's [z_coord, mass]
+    """
+    tot_moi_xy = 0
+    overall_com = calculate_com()
+    for component, values in ComponentType.components.items():
+        component: ComponentType = component
+        difference = abs(overall_com - component.center_of_mass()[0])
+        tot_moi_xy += component.moment_of_inertia_xy() + (component.center_of_mass()[1] * (difference ** 2))
+
+    return tot_moi_xy
+
+
+def total_moi_z():
+    """
+    Calculates the total moment of inertia about the center of mass in the Z Plane, about the central axis of the rocket. 
+    """
+    tot_moi_z = 0
+    for component, values in ComponentType.components.items():
+        component: ComponentType = component
+        tot_moi_z += component.moment_of_inertia_z()
+
+    return tot_moi_z
+
+
+def calculate_com():
+    """
+    Determines the center of mass of the system (entire rocket)
+    Iterates through [Z-Coordinate of center of mass, mass] for each component of the rocket.
+    Returns the Z-Coordinate of the center of mass of the rocket, given Z = 0 is the bottom of the rocket.
+    """
+    total_mass_x_distance = 0
+    total_mass = 0
+    for component, values in ComponentType.components.items():
+        component: ComponentType = component
+        individual_com = component.center_of_mass() #2-element list, [Z-Coordinate of center of mass, mass]
+        total_mass_x_distance += individual_com[0] * individual_com[1] #numerator
+        total_mass += individual_com[1] #add mass to total mass, denominator
+    rocket_center_of_mass = total_mass_x_distance / total_mass #calculate overall center-of-mass
+    return rocket_center_of_mass   
+
+
+
+def fuel_proportion_remaining():
+    """
+    Given the integration of the throttle curve from t-initial to t-current,
+    and the integration of the thrust curve from t-initial to t-final,
+    returns the percent of fuel remaining.
+    """
+    return 1 - (np.sum(Engine.throttle_history) / np.sum(Engine.thrust_curve)) #Ask Luke?
+
+
+def update_lengthwise_fuel(fuel_prop_remaining):
+    """
+    Updates the length of the two fuel cylinders that are emptying lengthwise (cylinder's height is decreasing)
+    These are the components of type 'ChangingSolidCylinder'
+    New Length = Current Length * Fuel Proportion Remaining
+    ChangingSolidCylinder List Format: [mass, radius, current length, bottom_z, original_length]
+    """
+    for component, values in ComponentType.components.items():
+        component: ComponentType = component
+        if type(component) is ComponentType.ChangingSolidCylinder:
+            original_length = ComponentType.components[component][-1]
+            ComponentType.components[component][2] = original_length * fuel_prop_remaining
+
+def update_radial_fuel(fuel_prop_remaining):
+    """
+    Updates the inner radius of the paraffin wax fuel cylinder that empties from the inside-out (Cylinder's inner radius is increasing as rocket rockets (:))
+    These are the components of type 'ChangingHollowCylinder'
+    New Inner Radius = SQRT(outer_radius ** 2 - (new_volume / (PI * height)))
+    Changing Hollow Cylinder List Format: [mass, current_inner_radius, outer_radius, length, bottom_z, original_inner_radius]
+    """
+    for component, values in ComponentType.components.items():
+        component: ComponentType = component
+        if type(component) is ComponentType.ChangingHollowCylinder:
+            cyl_vars = ComponentType.components[component]
+            outer_rad = cyl_vars[2]
+            original_inner_rad = cyl_vars[5]
+            length = cyl_vars[3]
+
+            new_volume = fuel_prop_remaining * math.pi * (outer_rad ** 2 - original_inner_rad ** 2) * length
+            new_inner_rad = math.sqrt(outer_rad ** 2 - (new_volume / (math.pi * length)))
+            cyl_vars[1] = new_inner_rad
+
+
+#Throttle: Range 0 - 1 (Represents flow of oxygen out of bottom of rocket) Proportional
+#We need to determine how depleted the fuel chambers are at any given time
+#Length of "inner cylinders" change for two chambers, inner radius changes for last cylinder
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    
+    ComponentType.components[ComponentType.HollowCylinder] = [1, 2, 3, 4, 5]
+    ComponentType.components[ComponentType.SolidCylinder] = [1, 2, 3, 4]
+
+    #For a given timestep:
+    # STEPS (Updating MOI During Flight
+    # 1. Calculate fuel proportion remaining
+    current_fuel_remaining = fuel_proportion_remaining()
+    # 2. Change fuel-component variables (using update)
+    update_lengthwise_fuel(current_fuel_remaining)
+    update_radial_fuel(current_fuel_remaining)
+    # 3. Calculate new COM (using calculate_com()) - happens inside the functions of step 4
+    # 4. Calculate new MOIs (total_moi_xy())
+    current_xy_moi = total_moi_xy()
+    current_z_moi = total_moi_z()
+
+
+    
